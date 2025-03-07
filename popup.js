f586dc284dd4b58ad79f1ea96aead65aea0d043c
Alessandro Mingione <thisisalessandro@gmail.com>
2025-03-07 12:05:10 -0800

    First pass at styling

--- popup.js
+++ popup.js
@@ -1,66 +1,74 @@
 var lastStatus;
 
 function browseToURL() {
-    if (lastStatus && lastStatus.browseToURL) {
-        chrome.tabs.create({ url: lastStatus.browseToURL });
-    }
+  if (lastStatus && lastStatus.browseToURL) {
+    chrome.tabs.create({ url: lastStatus.browseToURL });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    let btn = document.getElementById("button");
-    let st = document.getElementById("state");
+  let btn = document.getElementById("button");
+  let st = document.getElementById("state");
+  let tail = document.getElementById("tailnet");
 
-    let port = chrome.runtime.connect({ name: "popup" });
+  let port = chrome.runtime.connect({ name: "popup" });
 
-    port.onMessage.addListener((msg) => {
-        console.log("Received from background:", JSON.stringify(msg))
-        if (msg.installCmd) {
-            st.innerHTML = "<b>Installation needed. Run:</b><pre>" + msg.installCmd + "</pre>";
-            btn.hidden = true;
-            return;
+  port.onMessage.addListener((msg) => {
+    console.log("Received from background:", JSON.stringify(msg));
+    if (msg.installCmd) {
+      st.innerHTML =
+        "<b>Installation needed. Run:</b><pre>" + msg.installCmd + "</pre>";
+      btn.hidden = true;
+      return;
+    }
+    if (msg.error) {
+      console.log("Error from background:", msg);
+      st.innerText = msg.error;
+      btn.hidden = true;
+      return;
+    }
+    let sm = msg.status;
+    if (sm) {
+      lastStatus = sm;
+      console.log("Status from background:", JSON.stringify(sm));
+      if (sm.error) {
+        console.log("Status error:", sm.error);
+        st.innerText = sm.error;
+        btn.hidden = true;
+        return;
+      }
+      if (sm.needsLogin) {
+        if (sm.browseToURL) {
+          st.innerHTML = "<b><a href='#login'>Log in</a>.</b>";
+          st.querySelector("a").onclick = browseToURL;
+        } else {
+          st.innerHTML = "<b>Login required; no URL</b>";
         }
-        if (msg.error) {
-            console.log("Error from background:", msg);
-            st.innerText = msg.error;
-            btn.hidden = true;
-            return;
-        }
-        let sm = msg.status;
-        if (sm) {
-            lastStatus = sm;
-            console.log("Status from background:", JSON.stringify(sm));
-            if (sm.error) {
-                console.log("Status error:", sm.error);
-                st.innerText = sm.error;
-                btn.hidden = true;
-                return;
-            }
-            if (sm.needsLogin) {
-                if (sm.browseToURL) {
-                    st.innerHTML = "<b><a href='#login'>Log in</a>.</b>";
-                    st.querySelector('a').onclick = browseToURL;
-                } else {
-                    st.innerHTML = "<b>Login required; no URL</b>";
-                }
-                btn.hidden = true;
-                return;
-            }
-            st.innerText = (sm.running ? "🟢" : "🔴") + " " + sm.tailnet;
-            btn.hidden = false;
-            btn.innerText = "Settings";
-            return
-        }
+        btn.hidden = true;
+        return;
+      }
+      st.innerHTML = sm.running
+        ? '<div class="statusdot on"></div>Connected'
+        : '<div class="statusdot off"></div>Not connected';
+      tail.innerHTML = sm.tailnet;
+      btn.hidden = false;
+      btn.innerText = "Settings";
+      return;
+    }
 
-        st.innerText = msg;
-    });
-        
-    window.onunload = () => { port.disconnect(); }; // probably redundant
+    st.innerText = msg;
+  });
 
-    btn.addEventListener("click", () => {
-        if (btn.innerText == "Settings") { // trashy :)
-            chrome.tabs.create({ url: "http://100.100.100.100" });
-            return
-        }
-        port.postMessage({ command: "toggleProxy" });
-    });
-})
+  window.onunload = () => {
+    port.disconnect();
+  }; // probably redundant
+
+  btn.addEventListener("click", () => {
+    if (btn.innerText == "Settings") {
+      // trashy :)
+      chrome.tabs.create({ url: "http://100.100.100.100" });
+      return;
+    }
+    port.postMessage({ command: "toggleProxy" });
+  });
+});
